e3d7f5a8a2919a4bfb50ce32646c9413a8978058
Eugen Nastyushkin <slevinzp@mail.ru>
2016-03-27 16:00:51 +0300

    4 controllers

---
+++ task1/script/main.js
@@ -0,0 +1,13 @@
+angular.module('task1', [])
+    .controller('ParentCtrl1', ['$scope', function($scope) {
+
+    }])
+    .controller('ParentCtrl2',['$scope', function($scope){
+
+    }])
+    .controller('ChildCtrl1',['$scope', function($scope){
+
+    }])
+    .controller('ChildCtrl2',['$scope', function($scope){
+
+    }]);
