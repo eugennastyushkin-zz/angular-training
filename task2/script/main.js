e945af5af477394a43dbac84c331a957a1c5824b
Eugen Nastyushkin <Eugennastyushkin@coherentsolutions.com>
2016-04-06 14:41:40 +0300

    task 2 complete

--- task2/script/main.js
+++ task2/script/main.js
@@ -1,5 +1,29 @@
 var app = angular.module('task2', []);
 
+app.controller('MainCtrl', function($scope, $http){
+    $scope.content = "";
+    $scope.setContent = function(tabNumber){
+        debugger;
+        alert(tabNumber);
+    };
+
+    $scope.onTabActivatedhandlerFirst = function(pane, evt){
+        console.log("asdasdasdas");
+    };
+
+    $scope.onTabActivatedhandlerSecond = function(pane, evt){
+        $scope.setContent(2);
+    };
+
+    $scope.onTabActivatedhandlerThird = function(pane, evt){
+        $http.get('http://vk.com').success(function(result){
+            $scope.content = result;
+        }).error(function(error, status, handler, request){
+            debugger;
+            $scope.content = request.url;
+        })
+    }
+});
 app.directive('tabControl', function(){
     return {
         restrict: 'E',
@@ -26,13 +50,18 @@ app.directive('tabControl', function(){
     };
 });
 
+var setContent = function(tabNumber){
+    alert(tabNumber);
+};
+
 app.directive('tab', function(){
     return {
         require: '^^tabControl',
         restrict: 'E',
         transclude: true,
         scope: {
-            title:'@'
+            title:'@',
+            onTabActivated: '&'
         },
         templateUrl: 'directive/tab.html',
         link: function(scope, element, attrs, tabControlController){
